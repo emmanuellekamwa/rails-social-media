e9c59a4c09956f4ecc8928084df874a1c373535b
emmanuellekamwa <emmanuellekamwa@gmail.com>
2021-11-25 14:50:16 +0100

    Add API for list of posts

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::PostsController < Api::V1::ApiController
+    before_action :authenticate_user!
+  
+    def show
+      @post = Post.find(params[:id])
+      render json: @post, status: 200
+    end
+  
+    def index
+      @posts = Post.all
+      render json: @posts
+    end
+  
+    def create
+      @post = current_user.posts.new(post_params)
+  
+      if @post.save
+        render json: @post, status: 201
+      else
+        render json: { errors: @post.errors }, status: 422
+      end
+    end
+  
+    private
+  
+    def post_params
+      params.require(:post).permit(:content)
+    end
+  end
